b7f8c343b0467622f4870a619a6fc4f38e976312
Annie-Cho <ani100482@gmail.com>
2022-07-06 09:47:52 +0900

    오타 수정

--- class/02-04-welcome-template-api/utils.js
+++ class/02-04-welcome-template-api/utils.js
@@ -3,7 +3,7 @@ export const getToday = () => {
     //월만 0부터 시작함!
     const yyyy = date.getFullYear()
     const mm = String(date.getMonth() + 1).padStart(2, "0")      //getMonth()는 0부터 시작해서 1월이 0임.. 대부분의 소문은 처음에 모르고 잘못 만들었다가 소문..
-    const dd = String(date.getDay()).padStart(2, "0")
+    const dd = String(date.getDate()).padStart(2, "0")
     const today = `${yyyy}-${mm}-${dd}`
 
     return today
