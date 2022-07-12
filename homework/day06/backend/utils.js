acaaf4da1728ffa959729dc71559d7864562aa9a
Annie-Cho <ani100482@gmail.com>
2022-07-12 09:27:31 +0900

    6일차 h/w 불필요한 기능 삭제

--- homework/day06/backend/utils.js
+++ homework/day06/backend/utils.js
@@ -8,13 +8,3 @@ export const getToday = () => {
 
     return today
 }
-
-export const getPhoneNum = () => {
-    const PhoneNumber01 = document.getElementById("PhoneNumber01").value
-    const PhoneNumber02 = document.getElementById("PhoneNumber02").value
-    const PhoneNumber03 = document.getElementById("PhoneNumber03").value
-
-    const phoneNum = PhoneNumber01 + PhoneNumber02 + PhoneNumber03
-    
-    return phoneNum
-}
