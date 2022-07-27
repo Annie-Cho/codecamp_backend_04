47c195fa2b1d8b777ea2492d41911b2882c1d510
Annie-Cho <ani100482@gmail.com>
2022-07-27 14:51:26 +0900

    [final-proj] added scope for numbers

--- final-project/src/apis/foods/dto/createFood.input.ts
+++ final-project/src/apis/foods/dto/createFood.input.ts
@@ -1,16 +1,19 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { Min } from 'class-validator';
 
 @InputType()
 export class CreateFoodInput {
   @Field(() => String)
   name: string;
 
+  @Min(0)
   @Field(() => Int)
   price: number;
 
   @Field(() => String)
   desc: string;
 
+  @Min(0)
   @Field(() => Int)
   kcal: number;
 }
