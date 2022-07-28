f03ebb4b5ef41b8c51ef530c98a1a5b1baf2e03e
Annie-Cho <ani100482@gmail.com>
2022-07-28 14:17:11 +0900

    [final-proj] mainCategories finished

---
+++ final-project/src/apis/mainCategories/mainCategories.resolver.ts
@@ -0,0 +1,50 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateMainCategoryInput } from './dto/createMainCategory.input';
+import { UpdateMainCategoryInput } from './dto/updateMainCategory.input';
+import { MainCategory } from './entities/mainCategory.entity';
+import { MainCategoriesService } from './mainCategories.service';
+
+@Resolver()
+export class MainCategoriesResolver {
+  constructor(private readonly mainCategoriesService: MainCategoriesService) {}
+
+  @Query(() => [MainCategory])
+  fetchMainCategories() {
+    return this.mainCategoriesService.findAll();
+  }
+
+  @Query(() => [MainCategory])
+  fetchMainCategoriesWithDeleted() {
+    return this.mainCategoriesService.findAllWithDeleted();
+  }
+
+  @Mutation(() => MainCategory)
+  createMainCategory(
+    @Args('createMainCategoryInput')
+    createMainCategoryInput: CreateMainCategoryInput,
+  ) {
+    return this.mainCategoriesService.create({ createMainCategoryInput });
+  }
+
+  @Mutation(() => MainCategory)
+  updateMainCategory(
+    @Args('mainCategoryId') mainCategoryId: string,
+    @Args('updateMainCategoryInput')
+    updateMainCategoryInput: UpdateMainCategoryInput,
+  ) {
+    return this.mainCategoriesService.update({
+      mainCategoryId,
+      updateMainCategoryInput,
+    });
+  }
+
+  @Mutation(() => Boolean)
+  deleteMainCategory(@Args('mainCategoryId') mainCategoryId: string) {
+    return this.mainCategoriesService.delete({ mainCategoryId });
+  }
+
+  @Mutation(() => Boolean)
+  restoreMainCategory(@Args('mainCategoryId') mainCategoryId: string) {
+    return this.mainCategoriesService.restore({ mainCategoryId });
+  }
+}
